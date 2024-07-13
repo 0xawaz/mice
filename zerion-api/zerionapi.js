891f97962b8e54a07af6d32a631162422b899752
0xawaz <amina.waddiz@gmail.com>
2024-07-13 22:15:58 +0200

    update auth to base64

--- zerion-api/zerionapi.js
+++ zerion-api/zerionapi.js
@@ -1,29 +1,30 @@
 const axios = require('axios');
-const base64 = require('base-64');
+require('dotenv').config();  // Ensure environment variables are loaded
 
 // Your Zerion API Key
-const apiKey = 'zk_dev_db9e3707de844df987cf5caf51fee02b';
+const apiKey = process.env.ZK_DEV_API_KEY;
+const wallet = process.env.WALLET;
 
 // Endpoint to fetch the wallet portfolio
-const endpoint = 'https://api.zerion.io/v1/wallets/0x11096722c0368f4C4343eA544033cF519eFb9275/portfolio';
-
-// Encoding the API key for Basic Auth
-const authValue = base64.encode(`${apiKey}:`);
+const endpoint = `https://api.zerion.io/v1/wallets/${wallet}/portfolio`;
 
 const headers = {
     'Accept': 'application/json',
-    'Authorization': `Basic ${authValue}`
-};
+    authorization: 'Basic emtfZGV2X2RiOWUzNzA3ZGU4NDRkZjk4N2NmNWNhZjUxZmVlMDJiOg=='};
 
 const fetchPortfolio = async () => {
     try {
-        const response = await axios.get(endpoint, { headers });
+        const response = await axios.get(endpoint, {
+            headers
+        });
         if (response.status === 200) {
             const data = response.data;
-            console.log("Total Portfolio Value (USD):", data.data.attributes.total_value_usd);
+
+            console.log("Total Portfolio Value (USD):", data.data.attributes.positions_distribution_by_type.wallet);
+
         } else {
             console.log("Failed to fetch data:", response.status);
-            console.log("Response:", response.data);
+            // console.log("Response:", response.data);
         }
     } catch (error) {
         console.error("Error fetching portfolio data:", error);
