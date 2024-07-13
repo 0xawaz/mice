270b5cc12a085aaf255a6cb82284f2afc02fa509
Aladeenb <alabtk@gmail.com>
2024-07-14 01:37:08 +0200

    Added contract utility functions

--- webapp/packages/nextjs/utils/scaffold-eth/contract.ts
+++ webapp/packages/nextjs/utils/scaffold-eth/contract.ts
@@ -1,315 +1,122 @@
-import { MutateOptions } from "@tanstack/react-query";
-import {
-  Abi,
-  AbiParameter,
-  AbiParameterToPrimitiveType,
-  AbiParametersToPrimitiveTypes,
-  ExtractAbiEvent,
-  ExtractAbiEventNames,
-  ExtractAbiFunction,
-} from "abitype";
-import type { ExtractAbiFunctionNames } from "abitype";
-import type { Simplify } from "type-fest";
-import type { MergeDeepRecord } from "type-fest/source/merge-deep";
-import {
-  Address,
-  Block,
-  GetEventArgs,
-  GetTransactionReceiptReturnType,
-  GetTransactionReturnType,
-  Log,
-  TransactionReceipt,
-  WriteContractErrorType,
-} from "viem";
-import { Config, UseReadContractParameters, UseWatchContractEventParameters } from "wagmi";
-import { WriteContractParameters, WriteContractReturnType } from "wagmi/actions";
-import { WriteContractVariables } from "wagmi/query";
-import deployedContractsData from "~~/contracts/deployedContracts";
-import externalContractsData from "~~/contracts/externalContracts";
-import scaffoldConfig from "~~/scaffold.config";
+import { ethers } from 'ethers';
+import { artifacts } from "hardhat";
 
-type AddExternalFlag<T> = {
-  [ChainId in keyof T]: {
-    [ContractName in keyof T[ChainId]]: T[ChainId][ContractName] & { external?: true };
-  };
-};
-
-const deepMergeContracts = <L extends Record<PropertyKey, any>, E extends Record<PropertyKey, any>>(
-  local: L,
-  external: E,
-) => {
-  const result: Record<PropertyKey, any> = {};
-  const allKeys = Array.from(new Set([...Object.keys(external), ...Object.keys(local)]));
-  for (const key of allKeys) {
-    if (!external[key]) {
-      result[key] = local[key];
-      continue;
-    }
-    const amendedExternal = Object.fromEntries(
-      Object.entries(external[key] as Record<string, Record<string, unknown>>).map(([contractName, declaration]) => [
-        contractName,
-        { ...declaration, external: true },
-      ]),
-    );
-    result[key] = { ...local[key], ...amendedExternal };
+// Declare the ethereum property on the window object
+declare global {
+  interface Window {
+    ethereum?: ethers.providers.ExternalProvider;
   }
-  return result as MergeDeepRecord<AddExternalFlag<L>, AddExternalFlag<E>, { arrayMergeMode: "replace" }>;
-};
-
-const contractsData = deepMergeContracts(deployedContractsData, externalContractsData);
-
-export type InheritedFunctions = { readonly [key: string]: string };
-
-export type GenericContract = {
-  address: Address;
-  abi: Abi;
-  inheritedFunctions?: InheritedFunctions;
-  external?: true;
-};
-
-export type GenericContractsDeclaration = {
-  [chainId: number]: {
-    [contractName: string]: GenericContract;
-  };
-};
-
-export const contracts = contractsData as GenericContractsDeclaration | null;
-
-type ConfiguredChainId = (typeof scaffoldConfig)["targetNetworks"][0]["id"];
-
-type IsContractDeclarationMissing<TYes, TNo> = typeof contractsData extends { [key in ConfiguredChainId]: any }
-  ? TNo
-  : TYes;
-
-type ContractsDeclaration = IsContractDeclarationMissing<GenericContractsDeclaration, typeof contractsData>;
-
-type Contracts = ContractsDeclaration[ConfiguredChainId];
-
-export type ContractName = keyof Contracts;
-
-export type Contract<TContractName extends ContractName> = Contracts[TContractName];
-
-type InferContractAbi<TContract> = TContract extends { abi: infer TAbi } ? TAbi : never;
-
-export type ContractAbi<TContractName extends ContractName = ContractName> = InferContractAbi<Contract<TContractName>>;
-
-export type AbiFunctionInputs<TAbi extends Abi, TFunctionName extends string> = ExtractAbiFunction<
-  TAbi,
-  TFunctionName
->["inputs"];
-
-export type AbiFunctionArguments<TAbi extends Abi, TFunctionName extends string> = AbiParametersToPrimitiveTypes<
-  AbiFunctionInputs<TAbi, TFunctionName>
->;
-
-export type AbiFunctionOutputs<TAbi extends Abi, TFunctionName extends string> = ExtractAbiFunction<
-  TAbi,
-  TFunctionName
->["outputs"];
-
-export type AbiFunctionReturnType<TAbi extends Abi, TFunctionName extends string> = IsContractDeclarationMissing<
-  any,
-  AbiParametersToPrimitiveTypes<AbiFunctionOutputs<TAbi, TFunctionName>> extends readonly [any]
-    ? AbiParametersToPrimitiveTypes<AbiFunctionOutputs<TAbi, TFunctionName>>[0]
-    : AbiParametersToPrimitiveTypes<AbiFunctionOutputs<TAbi, TFunctionName>>
->;
-
-export type AbiEventInputs<TAbi extends Abi, TEventName extends ExtractAbiEventNames<TAbi>> = ExtractAbiEvent<
-  TAbi,
-  TEventName
->["inputs"];
-
-export enum ContractCodeStatus {
-  "LOADING",
-  "DEPLOYED",
-  "NOT_FOUND",
 }
 
-type AbiStateMutability = "pure" | "view" | "nonpayable" | "payable";
-export type ReadAbiStateMutability = "view" | "pure";
-export type WriteAbiStateMutability = "nonpayable" | "payable";
+// Contract details
+const contractName = "ZKBounty";
+const contractAddress = '0x1085CC7f389d2ee9D20F4cd4b06984aC1Aa00bdF';
 
-export type FunctionNamesWithInputs<
-  TContractName extends ContractName,
-  TAbiStateMutibility extends AbiStateMutability = AbiStateMutability,
-> = Exclude<
-  Extract<
-    ContractAbi<TContractName>[number],
-    {
-      type: "function";
-      stateMutability: TAbiStateMutibility;
-    }
-  >,
-  {
-    inputs: readonly [];
+// Linea network details
+const LINEA_TESTNET_RPC_URL = "https://rpc.goerli.linea.build";
+const LINEA_TESTNET_CHAIN_ID = 59140;
+
+// Function to get provider
+function getProvider(): ethers.providers.Provider {
+  if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+    // We are in the browser and MetaMask is running
+    return new ethers.providers.Web3Provider(window.ethereum);
+  } else {
+    // We are on the server *OR* the user is not running MetaMask
+    return new ethers.providers.JsonRpcProvider(LINEA_TESTNET_RPC_URL, LINEA_TESTNET_CHAIN_ID);
   }
->["name"];
+}
 
-type Expand<T> = T extends object ? (T extends infer O ? { [K in keyof O]: O[K] } : never) : T;
-
-type UnionToIntersection<U> = Expand<(U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never>;
-
-type OptionalTupple<T> = T extends readonly [infer H, ...infer R] ? readonly [H | undefined, ...OptionalTupple<R>] : T;
-
-type UseScaffoldArgsParam<
-  TContractName extends ContractName,
-  TFunctionName extends ExtractAbiFunctionNames<ContractAbi<TContractName>>,
-> = TFunctionName extends FunctionNamesWithInputs<TContractName>
-  ? {
-      args: OptionalTupple<UnionToIntersection<AbiFunctionArguments<ContractAbi<TContractName>, TFunctionName>>>;
-      value?: ExtractAbiFunction<ContractAbi<TContractName>, TFunctionName>["stateMutability"] extends "payable"
-        ? bigint | undefined
-        : undefined;
-    }
-  : {
-      args?: never;
-    };
-
-export type UseScaffoldReadConfig<
-  TContractName extends ContractName,
-  TFunctionName extends ExtractAbiFunctionNames<ContractAbi<TContractName>, ReadAbiStateMutability>,
-> = {
-  contractName: TContractName;
-  watch?: boolean;
-} & IsContractDeclarationMissing<
-  Partial<UseReadContractParameters>,
-  {
-    functionName: TFunctionName;
-  } & UseScaffoldArgsParam<TContractName, TFunctionName> &
-    Omit<UseReadContractParameters, "chainId" | "abi" | "address" | "functionName" | "args">
->;
-
-export type ScaffoldWriteContractVariables<
-  TContractName extends ContractName,
-  TFunctionName extends ExtractAbiFunctionNames<ContractAbi<TContractName>, WriteAbiStateMutability>,
-> = IsContractDeclarationMissing<
-  Partial<WriteContractParameters>,
-  {
-    functionName: TFunctionName;
-  } & UseScaffoldArgsParam<TContractName, TFunctionName> &
-    Omit<WriteContractParameters, "chainId" | "abi" | "address" | "functionName" | "args">
->;
-
-type WriteVariables = WriteContractVariables<Abi, string, any[], Config, number>;
-
-export type TransactorFuncOptions = {
-  onBlockConfirmation?: (txnReceipt: TransactionReceipt) => void;
-  blockConfirmations?: number;
-};
-
-export type ScaffoldWriteContractOptions = MutateOptions<
-  WriteContractReturnType,
-  WriteContractErrorType,
-  WriteVariables,
-  unknown
-> &
-  TransactorFuncOptions;
-
-export type UseScaffoldEventConfig<
-  TContractName extends ContractName,
-  TEventName extends ExtractAbiEventNames<ContractAbi<TContractName>>,
-  TEvent extends ExtractAbiEvent<ContractAbi<TContractName>, TEventName> = ExtractAbiEvent<
-    ContractAbi<TContractName>,
-    TEventName
-  >,
-> = {
-  contractName: TContractName;
-  eventName: TEventName;
-} & IsContractDeclarationMissing<
-  Omit<UseWatchContractEventParameters, "onLogs" | "address" | "abi" | "eventName"> & {
-    onLogs: (
-      logs: Simplify<
-        Omit<Log<bigint, number, any>, "args" | "eventName"> & {
-          args: Record<string, unknown>;
-          eventName: string;
-        }
-      >[],
-    ) => void;
-  },
-  Omit<UseWatchContractEventParameters<ContractAbi<TContractName>>, "onLogs" | "address" | "abi" | "eventName"> & {
-    onLogs: (
-      logs: Simplify<
-        Omit<Log<bigint, number, false, TEvent, false, [TEvent], TEventName>, "args"> & {
-          args: AbiParametersToPrimitiveTypes<TEvent["inputs"]> &
-            GetEventArgs<
-              ContractAbi<TContractName>,
-              TEventName,
-              {
-                IndexedOnly: false;
-              }
-            >;
-        }
-      >[],
-    ) => void;
+// Function to get signer
+async function getSigner(): Promise<ethers.Signer> {
+  const provider = getProvider();
+  if (provider instanceof ethers.providers.Web3Provider) {
+    // Request account access if needed
+    await provider.send('eth_requestAccounts', []);
+    return provider.getSigner();
+  } else {
+    throw new Error("MetaMask not detected. Please use a Web3-enabled browser.");
   }
->;
+}
 
-type IndexedEventInputs<
-  TContractName extends ContractName,
-  TEventName extends ExtractAbiEventNames<ContractAbi<TContractName>>,
-> = Extract<AbiEventInputs<ContractAbi<TContractName>, TEventName>[number], { indexed: true }>;
+// Function to get contract instance
+async function getContract(): Promise<ethers.Contract> {
+  const signer = await getSigner();
+  const ZKBountyArtifact = await artifacts.readArtifact(contractName);
+  return new ethers.Contract(contractAddress, ZKBountyArtifact.abi, signer);
+}
 
-export type EventFilters<
-  TContractName extends ContractName,
-  TEventName extends ExtractAbiEventNames<ContractAbi<TContractName>>,
-> = IsContractDeclarationMissing<
-  any,
-  IndexedEventInputs<TContractName, TEventName> extends never
-    ? never
-    : {
-        [Key in IsContractDeclarationMissing<
-          any,
-          IndexedEventInputs<TContractName, TEventName>["name"]
-        >]?: AbiParameterToPrimitiveType<Extract<IndexedEventInputs<TContractName, TEventName>, { name: Key }>>;
-      }
->;
+// Utility functions to interact with the contract
 
-export type UseScaffoldEventHistoryConfig<
-  TContractName extends ContractName,
-  TEventName extends ExtractAbiEventNames<ContractAbi<TContractName>>,
-  TBlockData extends boolean = false,
-  TTransactionData extends boolean = false,
-  TReceiptData extends boolean = false,
-> = {
-  contractName: TContractName;
-  eventName: IsContractDeclarationMissing<string, TEventName>;
-  fromBlock: bigint;
-  filters?: EventFilters<TContractName, TEventName>;
-  blockData?: TBlockData;
-  transactionData?: TTransactionData;
-  receiptData?: TReceiptData;
-  watch?: boolean;
-  enabled?: boolean;
-};
+export async function registerIssuer(): Promise<ethers.ContractReceipt> {
+  const contract = await getContract();
+  const tx = await contract.registerIssuer();
+  return tx.wait();
+}
 
-export type UseScaffoldEventHistoryData<
-  TContractName extends ContractName,
-  TEventName extends ExtractAbiEventNames<ContractAbi<TContractName>>,
-  TBlockData extends boolean = false,
-  TTransactionData extends boolean = false,
-  TReceiptData extends boolean = false,
-  TEvent extends ExtractAbiEvent<ContractAbi<TContractName>, TEventName> = ExtractAbiEvent<
-    ContractAbi<TContractName>,
-    TEventName
-  >,
-> =
-  | IsContractDeclarationMissing<
-      any[],
-      {
-        log: Log<bigint, number, false, TEvent, false, [TEvent], TEventName>;
-        args: AbiParametersToPrimitiveTypes<TEvent["inputs"]> &
-          GetEventArgs<
-            ContractAbi<TContractName>,
-            TEventName,
-            {
-              IndexedOnly: false;
-            }
-          >;
-        block: TBlockData extends true ? Block<bigint, true> : null;
-        receipt: TReceiptData extends true ? GetTransactionReturnType : null;
-        transaction: TTransactionData extends true ? GetTransactionReceiptReturnType : null;
-      }[]
-    >
-  | undefined;
+export async function registerToBounty(bountyId: string) {
+  const contract = await getContract();
+  const tx = await contract.registerToBounty(bountyId);
+  return tx.wait();
+}
 
-export type AbiParameterTuple = Extract<AbiParameter, { type: "tuple" | `tuple[${string}]` }>;
+export async function submitReport(bountyId: string, reportHash: string) {
+  const contract = await getContract();
+  const tx = await contract.submitReport(bountyId, reportHash);
+  return tx.wait();
+}
+
+export async function finalizeReport(bountyId: string, hunter: string, hash: string) {
+  const contract = await getContract();
+  const tx = await contract.finalizeReport(bountyId, hunter, hash);
+  return tx.wait();
+}
+
+export async function withdrawUnapprovedBounty(bountyId: string) {
+  const contract = await getContract();
+  const tx = await contract.withdrawUnapprovedBounty(bountyId);
+  return tx.wait();
+}
+
+// Read functions
+
+export async function getBounty(bountyId: string) {
+  const contract = await getContract();
+  return contract.getBounty(bountyId);
+}
+
+export async function getBountyReward(bountyId: string) {
+  const contract = await getContract();
+  const reward = await contract.getBountyReward(bountyId);
+  return ethers.utils.formatEther(reward);
+}
+
+export async function getBountyIds() {
+  const contract = await getContract();
+  return contract.getBountyIds();
+}
+
+export async function getBountyAtIndex(index: number) {
+  const contract = await getContract();
+  return contract.getBountyAtIndex(index);
+}
+
+export async function getReportHash(bountyId: string, hunter: string) {
+  const contract = await getContract();
+  return contract.getReportHash(bountyId, hunter);
+}
+
+export async function getHuntersInBounty(bountyId: string) {
+  const contract = await getContract();
+  return contract.getHuntersInBounty(bountyId);
+}
+
+export async function getSubmittedReportsInBounty(bountyId: string) {
+  const contract = await getContract();
+  return contract.getSubmittedReportsInBounty(bountyId);
+}
+
+export async function isRegisteredIssuer(issuer: string) {
+  const contract = await getContract();
+  return contract.isRegisteredIssuer(issuer);
+}
